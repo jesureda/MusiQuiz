934727af797514ee3fd437ada1416c472e5964d8
JSureda <jesussureda@gmail.com>
2018-05-06 19:43:47 +0200

    Cambios y nuevas ventanas

--- app/src/main/java/com/example/jsureda/musiquiz/FragBotones.java
+++ app/src/main/java/com/example/jsureda/musiquiz/FragBotones.java
@@ -1,6 +1,7 @@
 package com.example.jsureda.musiquiz;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -15,6 +16,8 @@ import android.widget.TextView;
 public class FragBotones extends Fragment {
 
     int codigo;
+    TextView enunciado;
+    Button resA, resB, resC, resD;
     public FragBotones() {
     }
 
@@ -22,19 +25,26 @@ public class FragBotones extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView =inflater.inflate(R.layout.fragment_logo, container, false);
-        TextView enunciado = (TextView) rootView.findViewById(R.id.lblPregunta);
-        Button resA = (Button) rootView.findViewById(R.id.btnResA);
-        Button resB = (Button) rootView.findViewById(R.id.btnResB);
-        Button resC = (Button) rootView.findViewById(R.id.btnResC);
-        Button resD = (Button) rootView.findViewById(R.id.btnResD);
+        View rootView =inflater.inflate(R.layout.fragment_botones, container, false);
+        enunciado = (TextView) rootView.findViewById(R.id.lblPregunta);
+        resA = (Button) rootView.findViewById(R.id.btnResA);
+        resB = (Button) rootView.findViewById(R.id.btnResB);
+        resC = (Button) rootView.findViewById(R.id.btnResC);
+        resD = (Button) rootView.findViewById(R.id.btnResD);
 
+        resA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Niveles.class);
+                startActivity(intent);
 
-        // Inflate the layout for this fragment
-        Bundle bundle = this.getArguments();
+            }
+        });
+        //Inflate the layout for this fragment
+/*        Bundle bundle = this.getArguments();
         if (bundle != null) {
             codigo = bundle.getInt("id", 0);
-        }
+        }*/
         return rootView;
     }
 
--- app/src/main/java/com/example/jsureda/musiquiz/Preguntas.java
+++ app/src/main/java/com/example/jsureda/musiquiz/Preguntas.java
@@ -1,12 +1,14 @@
 package com.example.jsureda.musiquiz;
 
+import android.os.CountDownTimer;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Preguntas extends AppCompatActivity {
-
+    TextView txtTiempo;
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
     FragBotones botones = new FragBotones();
@@ -14,9 +16,19 @@ public class Preguntas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preguntas);
+        txtTiempo = (TextView)findViewById(R.id.txtTime);
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.frBotones, botones);
         fragmentTransaction.commit();
+        new CountDownTimer(30000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                txtTiempo.setText("Tiempo restante: " + millisUntilFinished / 1000);
+                //here you can have your logic to set text to edittext
+            }
+            public void onFinish() {
+                txtTiempo.setText("Fuera de tiempo!");
+            }
+        }.start();
     }
 }
